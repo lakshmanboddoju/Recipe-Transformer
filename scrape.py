24eae2f28808fe24ab7f68b512595d386db9c88a
Kelly Roemer <kellyroemer2019@u.northwestern.edu>
2018-03-06 14:20:12 -0600

    transformer

--- scrape.py
+++ scrape.py
@@ -6,29 +6,50 @@ import string
 import re
 import pprint
 #from urllib.request import urlopen
-import urllib # use this instead of line above
+import urllib
 from bs4 import BeautifulSoup
 
+#measurements=[teaspoon, tsp, tablespoon, tbl, tbs, tbsp, "fluid ounce", "fl oz", cup, c, 
+#	litre, litre, L, pound, lb, ounce, oz, mg, milligram, gram, g, kg, kilogram, mm, 
+#	millimeter, cm, centimeter, m, meter, inch, "in"]
 
-
-def scrape_recipe(url):
+def scrape_ingredients(url):
     webUrl = url
-    webFile = urllib.urlopen(webUrl) #add urllib right before urlopen
+    webFile = urllib.urlopen(webUrl)
     webHtml = webFile.read()
     soup = BeautifulSoup(webHtml,"html.parser")
     webAll = soup.findAll("label", {"ng-class": "{true: 'checkList__item'}[true]"})
     ingredients = []
     for item in webAll:
-        print (item['title'])
+        #print (item['title'])
         ingredients.append(item['title'])
     return ingredients
     
-scraped_ingredients = scrape_recipe("https://www.allrecipes.com/recipe/23735/buffalo-style-chicken-pizza/?internalSource=rotd&referringId=1036&referringContentType=recipe%20hub")
-
-#print scraped_ingredients
+def scrape_directions(url):
+    webUrl = url
+    webFile = urlopen(webUrl)
+    webHtml = webFile.read()
+    soup = BeautifulSoup(webHtml,"html.parser")
+    webAll = soup.findAll("span", {"class": "recipe-directions__list--item"})
+    directions = []
+    for description in webAll:
+        print (description.string)
+        directions.append(description.string)
+    return directions
+    
+def get_quantities(directs):
+	quantities =[]
+	
+	for i in directs:
+		p = re.compile(r'([0-9]+)\s?(([./0-9]+)?)')
+		#number = re.search("([0-9]+)\s?(([./0-9]+)?))", anything)
+		number = p.search(i)
+		quantities.append(number.group())
+	print (quantities)	
+	return quantities
 
 def transformer(list_of_ingredients):
-    meat = ['skinless, boneless chicken breast halves', 'beef', 'pork']
+    meat = ['chicken', 'beef', 'pork']
     alt = 'tofu blocks'
     for i in list_of_ingredients:
         for m in meat:
@@ -38,24 +59,8 @@ def transformer(list_of_ingredients):
                 return new_ingredient
             else:
                 return i
-            #how to delete the original ingredient
 
-def extract_quantity(list_of_ingredients):
-    quantity = []
-    measurement = []
-    for i in list_of_ingredients:
-        #temp = re.search('\d+', i) #  \( \) is how to find literal parentheses in regex
-        temp = re.search('\d+', i)
-        if temp:
-            quantity.append(temp.group(0))
-        else:
-           quantity.append(1)
-        
-    return quantity
-
-#look for all numbers in parentheses
-#look for number in ()
-
-print (extract_quantity(scraped_ingredients))
-
-print (transformer(scraped_ingredients))
+#get_quantities(scrape_ingredients("https://www.allrecipes.com/recipe/82487/chicken-avocado-pizza/?internalSource=staff%20pick&referringId=1036&referringContentType=recipe%20hub"))
+get_quantities(scrape_ingredients("https://www.allrecipes.com/recipe/8372/black-magic-cake/"))
+#print(scrape_ingredients("https://www.allrecipes.com/recipe/8372/black-magic-cake/"))
+#print(scrape_directions("https://www.allrecipes.com/recipe/8372/black-magic-cake/"))
