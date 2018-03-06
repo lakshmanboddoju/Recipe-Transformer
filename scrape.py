38a0d6bdef6316e1506c79f522903d3451eb0596
Kelly Roemer <kellyroemer2019@u.northwestern.edu>
2018-03-06 15:06:17 -0600

    Kelly - added basic transformer for meats
    definitely needs editing but the idea is there

--- scrape.py
+++ scrape.py
@@ -5,7 +5,8 @@ from nltk import ne_chunk, pos_tag, word_tokenize
 import string
 import re
 import pprint
-from urllib.request import urlopen
+#from urllib.request import urlopen
+import urllib
 from bs4 import BeautifulSoup
 
 #measurements=[teaspoon, tsp, tablespoon, tbl, tbs, tbsp, "fluid ounce", "fl oz", cup, c, 
@@ -14,13 +15,13 @@ from bs4 import BeautifulSoup
 
 def scrape_ingredients(url):
     webUrl = url
-    webFile = urlopen(webUrl)
+    webFile = urllib.urlopen(webUrl)
     webHtml = webFile.read()
     soup = BeautifulSoup(webHtml,"html.parser")
     webAll = soup.findAll("label", {"ng-class": "{true: 'checkList__item'}[true]"})
     ingredients = []
     for item in webAll:
-        #print (item['title'])
+        print (item['title']) 
         ingredients.append(item['title'])
     return ingredients
     
@@ -47,6 +48,27 @@ def get_quantities(directs):
 	print (quantities)	
 	return quantities
 
-get_quantities(scrape_ingredients("https://www.allrecipes.com/recipe/8372/black-magic-cake/"))
+def transformer(list_of_ingredients): #simple base transformer
+    meat = ['skinless, boneless chicken breast halves', 'pork', 'beef'] #substitute with meat.txt
+    alt = 'tofu blocks' #substitute with vegetarian alternatives
+    for i in list_of_ingredients:
+        for m in meat: 
+            if m in i:
+                #if meat in i
+                new_ingredient = i.replace(m, alt)
+                return new_ingredient
+            else: 
+                return i
+            #how to delete the last item in the list
+            #without print (item['title']) in scrape_ingredients, this only prints the ingredient that has been changed
+
+scraped_ingredients = scrape_ingredients("https://www.allrecipes.com/recipe/23735/buffalo-style-chicken-pizza/?internalSource=rotd&referringId=1036&referringContentType=recipe%20hub")
+
+print (transformer(scraped_ingredients))
+
+#get_quantities(scrape_ingredients("https://www.allrecipes.com/recipe/8372/black-magic-cake/"))
+
+#transformer(scrape_ingredients("https://www.allrecipes.com/recipe/8372/black-magic-cake/"))
+
 #print(scrape_ingredients("https://www.allrecipes.com/recipe/8372/black-magic-cake/"))
 #print(scrape_directions("https://www.allrecipes.com/recipe/8372/black-magic-cake/"))
